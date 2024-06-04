7e3753458309524c2ba175f58923a04fedc128d4
Hy <hynguyen2610@gmail.com>
2024-06-04 09:20:17 +0700

    client: display remain seconds to expires using timer

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,28 @@
-const OrderShow = () => {
-  return <div>OrderShow</div>
+import { useEffect, useState } from "react";
+
+const OrderShow = ({ order }) => {
+  const [timeleft, setTimeleft] = useState(0);
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const remainMiliSeconds = new Date(order.expiresAt) - new Date();
+      setTimeleft(Math.round(remainMiliSeconds / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+
+  return <div>{timeleft > 0 ? `${timeleft} seconds until order expires` : 'Expired'}</div>
+}
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+  return { order: data };
 }
 
 export default OrderShow;
