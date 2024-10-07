15d0bb41902a6fc995c012f601ba863ace673afa
Hy Nguyen <hynguyen2610@gmail.com>
2024-10-07 10:01:55 +0700

    Tiny changes in Navbar - App name added - Username located to the right side of nav

--- client/components/new-header.js
+++ client/components/new-header.js
@@ -1,39 +1,42 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
-export default ({currentUser}) => {
+export default ({ currentUser }) => {
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const links = [
-        { label: 'Sign Up', href: '/auth/signup', condition: !currentUser },
-        { label: 'Sign In', href: '/auth/signin', condition: !currentUser },
-        { label: 'Sell Ticket', href: '/tickets/new', condition: currentUser },
-        { label: 'My Orders', href: '/orders', condition: currentUser },
-        { label: 'Sign Out', href: '/auth/signout', condition: currentUser },
-      ]
-        .filter(linkConfig => linkConfig.condition) // Filter based on the condition
-        .map(({ label, href }) => (
-          <li key={href} className="nav-item">
-            <Link legacyBehavior className="nav-item" href={href}>
-              <a className={`nav-link ${router.pathname === href ? 'active' : ''}`}>{label}</a>
-            </Link>
-          </li>
-        ));
-
-    return(
-        <nav className="navbar navbar-expand-lg navbar-light">
-        <Link className="navbar-brand" href="/">
-        {currentUser ? currentUser.email : "Anonymous user"}
+  const links = [
+    { label: 'Sign Up', href: '/auth/signup', condition: !currentUser },
+    { label: 'Sign In', href: '/auth/signin', condition: !currentUser },
+    { label: 'Sell Ticket', href: '/tickets/new', condition: currentUser },
+    { label: 'My Orders', href: '/orders', condition: currentUser },
+    { label: 'Sign Out', href: '/auth/signout', condition: currentUser },
+  ]
+    .filter(linkConfig => linkConfig.condition) // Filter based on the condition
+    .map(({ label, href }) => (
+      <li key={href} className="nav-item">
+        <Link legacyBehavior className="nav-item" href={href}>
+          <a className={`nav-link ${router.pathname === href ? 'active' : ''}`}>{label}</a>
         </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-                {links}
-            </ul>
-        </div>
+      </li>
+    ));
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <Link className="navbar-brand" href="/">
+        Ticket System
+      </Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          {links}
+        </ul>
+      </div>
+      <div>
+        {currentUser ? currentUser.email : "Anonymous user"}
+      </div>
     </nav>
-    );
+  );
 }
