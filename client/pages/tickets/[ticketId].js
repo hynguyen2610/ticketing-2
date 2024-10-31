797ef58114beb4bf7473ac7daad7ad3ea22c431f
Hy Nguyen <hynguyen2610@gmail.com>
2024-11-01 04:47:04 +0700

    Redesign ticket detail page

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,6 +1,5 @@
 import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
-import TicketCarousel from '../../components/carousel/ticket-carousel';
 
 const TicketShow = ({ ticket }) => {
   const { doRequest, errors } = useRequest({
@@ -19,26 +18,23 @@ const TicketShow = ({ ticket }) => {
   };
 
   return (
-    <>
-      <div className="container mt-5">
-        
-        <div className="card">
-          <div className="card-header">
-            <h3 className="card-title">Ticket Details</h3>
-            <h5 className="card-title">Name: <span id="ticket-name">{ticket.title}</span></h5>
-            <h6 className="card-subtitle mb-2 text-muted">Price: $<span id="ticket-price">{ticket.price.toFixed(2)}</span></h6>
-            <button className="btn btn-primary" onClick={onClick}>
-              Purchase
-            </button>
+    <div className="container mt-5">
+      <h3>{ticket.title}</h3>
+      <h6 className="text-muted">Price: ${ticket.price.toFixed(2)}</h6>
+      <button className="btn btn-primary" onClick={onClick}>
+        Purchase
+      </button>
+
+      <div className="row mt-4">
+        {ticket.images.map((image, index) => (
+          <div className="col-md-4 mb-4" key={image}>
+            <img src={`/uploads/${image}`} className="img-fluid" alt={`Ticket Image ${index + 1}`} />
           </div>
-          <TicketCarousel ticket={ticket} />
-        </div>
+        ))}
       </div>
 
-      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    </>
+      {errors && <div className="alert alert-danger mt-3">{errors}</div>}
+    </div>
   );
 };
 
